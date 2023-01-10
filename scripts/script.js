03468ac55299be7e08b949caffb3806c60231f77
RayanSp <rayan.spall@hva.nl>
2023-01-10 20:02:32 +0100

    content van de test is nu volledig

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,32 @@
-document.querySelector('.testformulier');
+// document.querySelector('.testformulier');
 
-function prev(){
+// function prev(){
 
+// }
+
+// function next(){
+    
+// }
+
+
+let vraagNummer = 1
+showVraag(vraagNummer);
+
+function next(n) {
+    showVraag(vraagNummer += n)
 }
 
-function next(){
-    
+function showVraag(n) {
+    console.log(n)
+    let i;
+    let vragen = document.getElementsByClassName("formuliervraag");
+    console.log(vragen);
+    if (n > vragen.lenght) {vraagNummer = 1}
+
+    if (n < 1) {vraagNummer = vragen.length}
+    for (i = 0; i < vragen.length; i++) {
+        vragen[i].style.display = "none"
+    }
+
+    vragen[vraagNummer - 1].style.display = "block";
 }
